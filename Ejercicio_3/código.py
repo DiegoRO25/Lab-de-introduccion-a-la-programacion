3ad27053def6b17d016170fe111606c7a8af3c4c
DiegoRO25 <82487867+DiegoRO25@users.noreply.github.com>
2026-02-23 22:24:31 -0600

    Implement username and password validation logic
    Add user input validation for username and password.

--- Ejercicio_3/código.py
+++ Ejercicio_3/código.py
@@ -1,1 +1,20 @@
+intentos = 0
+while intentos < 3:
+    usuario = input("ingresa tu nombre de usuario: ")
+    contraseña = input("ingresa la contraseña: ")
+    
+    if len(usuario) == 0:
+        print ("El nombre de usuario no debe estar vacío") 
+        
+    if " " in usuario: 
+        print ("El nombre de usuario no debe contener espacios")    
 
+    if len(contraseña) < 8:
+        print("la contraseña debe tener al menos 8 caracteres")
+
+    if usuario == ("admin") and contraseña == ("Admin2026"): 
+        print("Se ingresó correctamente al sistema")
+        break
+    else:
+        intentos += 1
+        print(f"Intento número: {intentos}")
